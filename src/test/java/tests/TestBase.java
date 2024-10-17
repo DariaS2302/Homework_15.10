3336acc320ff98be0b80b119d62ac36757b55ad2
Sarycheva Daria <d.saricheva@gmail.com>
2024-10-17 10:23:11 +0300

    add BuildGradle6

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -22,7 +22,7 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browser = System.getProperty("browserNameAndVersion", "chrome:121.0");
+        Configuration.browser = System.getProperty("browserNameAndVersion", "chrome");
         Configuration.browserSize = System.getProperty("browserSize");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
